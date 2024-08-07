30aab5b85560a0743e89611fc64310250cde015a
HarryCode15 <harshbtekade137@gmail.com>
2024-08-08 01:24:31 +0530

    New project my travel list.

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+
+
+
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <img src="/images/logo.png" alt="Logo" className="navbar-logo"/>
+            <h1 className="navbar-title">my travel journal</h1>
+        </div>
+    )
+}
